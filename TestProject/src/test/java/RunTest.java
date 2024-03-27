836deacbb0def92ad8937d166907e516847bf626
boldhead666 <90863523+boldhead666@users.noreply.github.com>
2024-03-27 09:47:17 +0200

    Update RunTest.java

--- TestProject/src/test/java/RunTest.java
+++ TestProject/src/test/java/RunTest.java
@@ -5,6 +5,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class RunTest {
     @Test
     public void openTest(){
+        System.out.println("Работает");
         open("https://www.google.ru/?hl=ru");
     }
 }
