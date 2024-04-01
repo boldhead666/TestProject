52182b5d1b1cdf378a559e0363090ef12f363ac6
Sergey <s.lysenkov@zaymigo.ru>
2024-04-01 10:27:57 +0200

    ddd

--- TestProject/src/test/java/RunTest.java
+++ TestProject/src/test/java/RunTest.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class RunTest {
     @Test
     public void openTest(){
-        System.out.println("run");
+        System.out.println("Go");
         open("https://www.google.ru/?hl=ru");
     }
 }
